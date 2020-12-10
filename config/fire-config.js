2fe27d3dbfe83d4e10b013d08dfa6dac7f5cda8c
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-12-10 16:12:09 +0100

    done

--- config/fire-config.js
+++ config/fire-config.js
@@ -1,14 +1,20 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  // INSERT YOUR OWN CONFIG HERE
+	apiKey: 'AIzaSyCYOPWOIkNtuZm5CFRNiXiErQAuld5KV7o',
+	authDomain: 'blog-9c498.firebaseapp.com',
+	projectId: 'blog-9c498',
+	storageBucket: 'blog-9c498.appspot.com',
+	messagingSenderId: '68309631143',
+	appId: '1:68309631143:web:62a747b959f4deefc11b82'
 };
 
 try {
-  firebase.initializeApp(firebaseConfig);
-} catch(err){
-  if (!/already exists/.test(err.message)) {
-    console.error('Firebase initialization error', err.stack)}
+	firebase.initializeApp(firebaseConfig);
+} catch (err) {
+	if (!/already exists/.test(err.message)) {
+		console.error('Firebase initialization error', err.stack);
+	}
 }
 
 const fire = firebase;
--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,38 +1,31 @@
 import fire from '../../config/fire-config';
-import Link from 'next/link'
+import Link from 'next/link';
 
 const Blog = (props) => {
-
-  return (
-    <div>
-      <h2>{props.title}</h2>
-      <p>
-        {props.content}
-      </p>
-      <Link href="/">
-        <a>Back</a>
-      </Link>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h2>{props.title}</h2>
+			<p>{props.content}</p>
+			<Link href="/">
+				<a>Back</a>
+			</Link>
+		</div>
+	);
+};
 
 export const getServerSideProps = async ({ query }) => {
-  const content = {}
-  await fire.firestore()
-    .collection('blog')
-    .doc(query.id)
-    .get()
-    .then(result => {
-      content['title'] = result.data().title;
-      content['content'] = result.data().content;
-    });
+	const content = {};
+	await fire.firestore().collection('blog').doc(query.id).get().then((result) => {
+		content['title'] = result.data().title;
+		content['content'] = result.data().content;
+	});
 
-  return {
-    props: {
-      title: content.title,
-      content: content.content,
-    }
-  }
-}
+	return {
+		props: {
+			title: content.title,
+			content: content.content
+		}
+	};
+};
 
-export default Blog
+export default Blog;
